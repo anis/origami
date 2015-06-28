b35dfe7af7fb51eb01c1c78261d3285e531402e5
Anis Safine <anis@poensis.fr>
2015-06-28 10:41:18 +0000

    Implement the list of users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,11 +2,29 @@
 namespace Poensis\Origami\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityRepository;
 
 class UserController
 {
+    protected $users;
+
+    public function __construct(EntityRepository $users)
+    {
+        $this->users = $users;
+    }
+
     public function listAction()
     {
-        return new JsonResponse();
+        $users = $this->users->findAll();
+        $response = array();
+
+        foreach ($users as $user) {
+            $response[] = array(
+                'email'    => $user->getEmail(),
+                'password' => $user->getPassword(),
+            );
+        }
+
+        return new JsonResponse($response);
     }
 }
--- tests/unit/Controller/UserController.php
+++ tests/unit/Controller/UserController.php
@@ -3,6 +3,8 @@ namespace tests\unit\Poensis\Origami\Controller;
 
 use mageekguy\atoum;
 use Symfony\Component\HttpFoundation\Request;
+use Poensis\Origami\Entity\User;
+use Poensis\Origami\Controller\UserController as Controller;
 
 class UserController extends atoum
 {
@@ -22,6 +24,64 @@ class UserController extends atoum
             ->then
             ->object($response)
                 ->isInstanceOf('Symfony\Component\HttpFoundation\JsonResponse');
+    }
+
+    public function test___listAction___returnsAllUsers()
+    {
+        // Create the mock
+        $fakeUsers = array();
+        for ($i = 0; $i < 3; $i++) {
+            $fakeUsers[] = $this->createUser();
+        }
+
+        $mock = $this->mockRepository('Poensis\Origami\Repository\UserRepository');
+        $mock->getMockController()->findAll = function () use ($fakeUsers) {
+            return array_map(function ($user) { return $user['entity']; }, $fakeUsers);
+        };
+
+        // Test
+        $this
+            ->given($response = $this->get('/users/'))
+                ->and($content = json_decode($response->getContent(), true))
+            ->then
+                ->array($content)
+                    ->isIdenticalTo(array_map(function ($user) { return $user['raw']; }, $fakeUsers));
+    }
+
+    protected function createUser()
+    {
+        $user = new User();
+        $user->setEmail(uniqid());
+        $user->setPassword(uniqid());
+
+        return array(
+            'entity' => $user,
+            'raw'    => array(
+                'email'    => $user->getEmail(),
+                'password' => $user->getPassword(),
+            )
+        );
+    }
+
+    protected function mockRepository($repositoryClass)
+    {
+        if (!class_exists($repositoryClass)) {
+            $repositoryClass = 'Doctrine\ORM\EntityRepository';
+        }
+
+        $mockClass = '\mock\\' . $repositoryClass;
+
+        $this->mockGenerator->orphanize('__construct');
+        $this->mockGenerator->shuntParentClassCalls();
+        $mockRepository = new $mockClass();
+        $this->mockGenerator->unshuntParentClassCalls();
+
+        global $app;
+        $app['users'] = $app->factory(function () use ($mockRepository) {
+            return new Controller($mockRepository);
+        });
+
+        return $mockRepository;
     }
 
     protected function get($uri)
