60fdfa40d72005bc6b7104eb2d301a96e0bd4b0b
Anis Safine Laget <anis@users.noreply.github.com>
2018-12-06 21:39:27 +0100

    Remove credentials from versioning

--- src/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    database: {
-        user: 'postgres',
-        host: 'localhost',
-        database: 'origami',
-        password: 'ApqmwnxdR_1989P',
-        port: 5432,
-    },
-};
--- src/database/client.js
+++ src/database/client.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-const { database } = require('../config');
+const { database } = require('../config.json');
 
 const client = new Client(database);
 client.connect();
